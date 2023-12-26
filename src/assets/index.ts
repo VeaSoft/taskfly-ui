6f1329bd91f96ae510c27849089cb8ca6aa82f80
Covenant Chukwudi <covenantchukwudi@gmail.com>
2023-12-26 10:08:51 -0600

    set up assets used within the project

---
+++ src/assets/index.ts
@@ -0,0 +1,8 @@
+import heroImg from './images/Hero.svg'
+import heroimage from './images/heroimage.png'
+import BattleNet from './images/BattleNet.svg'
+import Man from './images/man.svg'
+import LoginScreen from './images/loginHome.svg'
+import google from './images/Google.svg'
+
+export { heroImg, heroimage, BattleNet, Man, LoginScreen, google  }
