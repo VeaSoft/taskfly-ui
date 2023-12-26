7597a8d19abaaee0a095b2fa1ac2ee837c527ee4
Covenant Chukwudi <covenantchukwudi@gmail.com>
2023-12-26 14:08:31 -0600

    working

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     open: false,
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 3000,
   },
   build: {
     outDir: "build",
