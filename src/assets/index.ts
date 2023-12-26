6d3850a339a0a54195e2909e1c5d59eb902c8c4c
Covenant Chukwudi <covenantchukwudi@gmail.com>
2023-12-26 13:19:16 -0600

    working

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,6 @@
 import heroImg from './images/Hero.svg'
 import heroimage from './images/heroimage.png'
-import BattleNet from './images/BattleNet.svg'
+import BattleNet from './images/Battlenet.svg'
 import Man from './images/man.svg'
 import LoginScreen from './images/loginHome.svg'
 import google from './images/Google.svg'
